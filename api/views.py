5d2bf8953aa31b7b862df985592c8362df0b2ca1
guillaumedsde <guillaume@desusanne.com>
2021-01-09 22:05:44 +0100

    feat: remove XML endpoint

--- api/views.py
+++ api/views.py
@@ -8,26 +8,6 @@ from api import models
 from rest_framework import permissions
 from api import serializers
 
-from rest_framework_xml.renderers import XMLRenderer
-
-
-class RootView(APIView):
-    permission_classes = [permissions.AllowAny]
-
-    http_method_names = ["get"]
-    renderer_classes = (XMLRenderer,)
-
-    def get(self, request, format=None):
-        print(request.query_params)
-        if request.query_params["t"] == "search":
-            torrents = self.search(request.query_params["q"])
-            serialized_torrents = serializers.TorrentSerializer(torrents, many=True)
-            print(type(serialized_torrents.data))
-            return Response(serialized_torrents.data)
-
-    def search(self, query_string):
-        return models.Torrent.objects.filter(name__search=query_string)
-
 
 class TorrentViewSet(viewsets.ReadOnlyModelViewSet):
 
