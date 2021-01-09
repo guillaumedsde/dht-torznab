dfc63711830cff92646dc3c1b1006ba72b58b97b
guillaumedsde <guillaume@desusanne.com>
2021-01-09 18:20:34 +0100

    feat: add basic search view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from django.contrib.auth.models import User
 
-# Create your views here.
+from api import models
+
+
+class RootView(APIView):
+    def get(self, request, format=None):
+        if request.query_params.t == "search":
+            return self.search(request.query_params.q)
+
+    def search(self, query_string):
+        return models.Torrent.objects.filter(name__search=query_string)
