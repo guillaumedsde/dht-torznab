e763c77c0f37b6612a807cf612425598af99aef1
guillaumedsde <guillaume@desusanne.com>
2021-01-10 01:14:14 +0100

    fix: define Base ID class as abstract

--- api/models.py
+++ api/models.py
@@ -6,6 +6,9 @@ from django.db import models
 class BaseModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
+    class Meta:
+        abstract = True
+
 
 class Torrent(BaseModel):
     name = models.TextField()
