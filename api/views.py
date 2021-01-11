0f2694ac28f89d53e8c15a971f6b6e21cfed85e7
guillaumedsde <guillaume@desusanne.com>
2021-01-11 22:24:39 +0100

    feat: prefetch related in TorrentViewSet

--- api/views.py
+++ api/views.py
@@ -4,7 +4,7 @@ from api import models, serializers
 
 
 class TorrentViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = models.Torrent.objects.all()
+    queryset = models.Torrent.objects.prefetch_related("files")
     serializer_class = serializers.TorrentSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ["name"]
