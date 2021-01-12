595804fd6eb48a375675843273c94e53e865ea36
guillaumedsde <guillaume@desusanne.com>
2021-01-12 23:37:01 +0100

    feat: add offset and capabilities check

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -5,22 +6,39 @@ from api import models
 
 
 def index(request, *args, **kwargs):
-    print(args)
-    print(kwargs)
     if function := request.GET.get("t", None):
         if function == "search":
+            offset = int(request.GET.get("offset", "0"))
+            limit = int(request.GET.get("limit", "25"))
             if query := request.GET.get("q", None):
-                torrents = models.Torrent.objects.filter(name__search=query)
-                return render(
-                    request,
-                    "index.xml",
-                    {
-                        "torrents": torrents,
-                        "category": function,
-                        "url": request.get_full_path(),
-                    },
-                    content_type="text/xml",
-                    status=200,
+                torrents = models.Torrent.objects.prefetch_related("files").filter(
+                    name__search=query
                 )
+            else:
+                torrents = models.Torrent.objects.prefetch_related("files").all()
+
+            paginator = Paginator(torrents, limit)
+            torrents_to_show = paginator.get_page(offset)
+            return render(
+                request,
+                "feed.xml",
+                {
+                    "torrents": torrents_to_show,
+                    "category": function,
+                    "url": request.get_full_path(),
+                    "offset": offset,
+                    "total": len(torrents)
+                },
+                content_type="text/xml",
+                status=200,
+            )
+        if function == "caps":
+            return render(
+                request,
+                "caps.xml",
+                {},
+                content_type="text/xml",
+                status=200,
+            )
 
     return HttpResponse("test")
