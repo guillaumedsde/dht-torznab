84cfe2ac0b0735855696bc82271291e18ffe55a9
guillaumedsde <guillaume@desusanne.com>
2021-01-30 13:00:51 +0100

    style: lint

--- api/views.py
+++ api/views.py
@@ -3,7 +3,6 @@ from typing import Optional, Tuple
 from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 from django.core.paginator import Paginator
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
 from lxml import etree as ET
 
 from api import models, torznab
