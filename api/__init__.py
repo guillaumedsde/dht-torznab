2a9b64be046a31a70cdf221d2269de70f89170f0
guillaumedsde <30023543+guillaumedsde@users.noreply.github.com>
2023-05-08 15:56:40 +0200

    refactor: cleanup repository

--- api/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Django Application for a torznab API.
-
-The applications also exposes a restful API to access torrents
-crawled with magneticod and ingested with django models using
-the listener.
-"""
--- docker/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-python /opt/app/manage.py migrate
-python /opt/app/manage.py runserver 0.0.0.0:8080
