66e262f51b2f72e9f4bd5ffe02397c803df44c2a
guillaumedsde <30023543+guillaumedsde@users.noreply.github.com>
2023-05-14 16:23:32 +0200

    chores: full text search setup

--- dht_torznab/models.py
+++ dht_torznab/models.py
@@ -1,6 +1,8 @@
 import sqlalchemy
+from sqlalchemy import Computed, Index
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from sqlalchemy.schema import ForeignKey, UniqueConstraint
+from sqlalchemy_utils import TSVectorType
 
 
 class Base(DeclarativeBase):
@@ -9,6 +11,7 @@ class Base(DeclarativeBase):
 
 UNIQUE_INFO_HASH_CONSTRAINT_NAME = "unique_info_hash"
 UNIQUE_TORRENT_ID_FILE_PATH_CONSTRAINT_NAME = "unique_torrent_id_file_path"
+PGSQL_DICTIONARY = "pg_catalog.simple"
 
 
 class Torrent(Base):
@@ -17,11 +20,17 @@ class Torrent(Base):
     info_hash: Mapped[bytes] = mapped_column()
     occurence_count: Mapped[int] = mapped_column(default=1)
 
+    search_vector = mapped_column(
+        TSVectorType("name", regconfig=PGSQL_DICTIONARY),
+        Computed(f"""to_tsvector('{PGSQL_DICTIONARY}', "name")""", persisted=True),
+    )
+
     files: Mapped[list["File"]] = relationship(back_populates="torrent")
 
     __tablename__ = "torrents"
     __table_args__ = (
         UniqueConstraint(info_hash, name=UNIQUE_INFO_HASH_CONSTRAINT_NAME),
+        Index("idx_torrent_name_tsv", search_vector, postgresql_using="gin"),
     )
 
 
