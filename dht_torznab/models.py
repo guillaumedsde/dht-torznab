bc09019a221b92003785322b9bfea61cd53079b1
guillaumedsde <30023543+guillaumedsde@users.noreply.github.com>
2023-05-27 00:36:28 +0200

    docs: FIXME

--- dht_torznab/models.py
+++ dht_torznab/models.py
@@ -22,6 +22,7 @@ class Base(DeclarativeBase):
     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(
         server_default=func.now(),
+        # FIXME: this does not seem to do anything?
         server_onupdate=func.now(),
     )
 
