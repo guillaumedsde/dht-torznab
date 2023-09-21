5afc50dad3eb6f2500f3678ec61bfc1c584019e2
guillaumedsde <30023543+guillaumedsde@users.noreply.github.com>
2023-09-21 21:49:18 +0200

    style: lint

--- dht_torznab/settings.py
+++ dht_torznab/settings.py
@@ -112,7 +112,7 @@ class Settings(pydantic.BaseSettings):
         env_prefix = "DHT_TORZNAB__"
         env_nested_delimiter = "__"
         # NOTE: see https://docs.pydantic.dev/1.10/usage/settings/#environment-variable-names
-        case_sensitive=True
+        case_sensitive = True
 
 
 @lru_cache
